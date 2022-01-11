e7f0c39441219a5fbadd1e30234c7d01d5c6e0c6
Samim Hossen <banglarcele1122@gmail.com>
2022-01-11 21:06:21 +0600

    2

--- APK File/app/src/main/java/com/example/al_quran/sura6.java
+++ APK File/app/src/main/java/com/example/al_quran/sura6.java
@@ -10,5 +10,6 @@ public class sura6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sura6);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
